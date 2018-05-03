b29964d4ed00ef2d2dd5302a7f209f19b88c4986
Paras <vijparas@gmail.com>
2018-05-03 22:34:20 +0530

    Added new Method for getting previous block and another function for getting the nonce

--- blockchain.py
+++ blockchain.py
@@ -17,3 +17,18 @@ class Blockchain:
         self.chain.append(block)
         return block
 
+    def getPreviousBlock(self):
+        return self.chain[-1]
+
+    def proofOfWork(self,previousProof):
+        newProof=1
+        checkProof=False
+        while checkProof is False:
+            hashOperation = hashlib.sha256(str(newProof**2-previousProof**2).encode()).hexdigest()
+            if(hashOperation[:4]=='0000'):
+                checkProof = True
+            else:
+                newProof+=1
+
+
+
